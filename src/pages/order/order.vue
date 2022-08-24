accc7b3c03b55de2049d775f3ac0163f6bd48594
Yangdada33 <1079487645@qq.com>
2022-08-24 19:22:51 +0800

    子父组件通信

--- src/pages/order/order.vue
+++ src/pages/order/order.vue
@@ -1,13 +1,39 @@
 <template>
   <div>
     <h1>订单管理</h1>
+    <!-- 方式一 -->
+    <!-- <Son :msg="msg" :num="num" :arr="arr" :obj="obj" :add="add" /> -->
+    <!-- 方式二 -->
+    <!-- <Son :msg="msg" :num="num" :arr="arr" :obj="obj" @add="add" /> -->
+    <!-- 方式三 -->
+    <Son :msg="msg" :num="num" :arr="arr" :obj="obj" :add="add" />
   </div>
 </template>
 <script>
+import Son from './son/idnex.vue'
 export default {
   //组件内守卫
   beforeRouteEnter(to, from, next) {
     next()
+  },
+  components: {
+    Son,
+  },
+  data() {
+    return {
+      msg: 'hellow body',
+      num: 1234,
+      arr: ['1', '2', '3', '45', '5'],
+      obj: {
+        name: '小赵',
+        ega: '28',
+      },
+    }
+  },
+  methods: {
+    add(value) {
+      console.log('测试', value)
+    },
   },
 }
 </script>
---
+++ src/pages/order/son/idnex.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <el-button @click="btn">点击</el-button>
+    <div>{{ num }}</div>
+    <div>{{ msg }}</div>
+    <div v-for="item in arr" :key="item.index">{{ item }}</div>
+    <div>{{ obj.name }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Son',
+  // props:[num],
+  props: {
+    num: {
+      type: Number,
+      default: '',
+    },
+    msg: {
+      type: String,
+      default: '',
+    },
+    arr: {
+      type: Array,
+      default: [],
+    },
+    obj: {
+      type: Object,
+      default: {},
+    },
+    //方式三
+    add: {
+      type: Function,
+      default: null,
+    },
+  },
+  methods: {
+    btn() {
+      console.log(1)
+      //方式一  子调父方法
+      // this.$parent.add('通过')
+
+      //方式二
+      // this.$emit('add', '通过')
+
+      //方式三
+      if (this.add) {
+        this.add('通过')
+      }
+    },
+  },
+}
+</script>
