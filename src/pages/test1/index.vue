20528b57be2286b7c35c20dc083693dd8c2bf67a
Yangdada33 <1079487645@qq.com>
2022-08-30 14:58:51 +0800

    添加vuex 获取值(之前不小心删掉了)

--- src/pages/test1/index.vue
+++ src/pages/test1/index.vue
@@ -1,3 +1,15 @@
 <template>
   <div>test1</div>
 </template>
+<script>
+export default {
+  computed: {
+    msg() {
+      return this.$store.getters['app/getMessage']
+    },
+  },
+  mounted() {
+    console.log('vuex', this.msg)
+  },
+}
+</script>
